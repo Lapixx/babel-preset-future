b0d2506770b9b7874eafc10b8cd251589cfe4ff6
Tijn Kersjes <tijn@divbyzero.nl>
2017-04-01 15:11:09 +0200

    Created preset file

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    require('babel-preset-es2015'),
+    require('babel-preset-es2016'),
+    require('babel-preset-es2017'),
+    require('babel-preset-stage-0')
+  ]
+};
